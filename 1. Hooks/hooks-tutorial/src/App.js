f75c07a79109291d46647ac2e0c87583e205bed7
njh0317 <njnjh0317@gmail.com>
2022-07-19 11:48:58 +0900

    [Add] useMemo, useCallback, custom Hook

--- 1. Hooks/hooks-tutorial/src/App.js
+++ 1. Hooks/hooks-tutorial/src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import React, { useState } from 'react';
 import './App.css';
 import Counter from './Counter';
 import Info from './Info';
+import Average from './Average';
 function App() {
   const [visible, setVisible] = useState(false);
   return (
--- 1. Hooks/hooks-tutorial/src/Info.js
+++ 1. Hooks/hooks-tutorial/src/Info.js
@@ -1,23 +1,14 @@
-import React, { useReducer } from "react";
+import React from "react";
+import useInput from "./useInputs";
 
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value
-  };
-}
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInput({
     name: '',
     nickname: ''
   });
 
   const {name, nickname} = state;
-  const onChange = e => {
-    dispatch(e.target);
-    console.log(e.target);
-  };
-  
+
   return(
     <div>
       <div>
